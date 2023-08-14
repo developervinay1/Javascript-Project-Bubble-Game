6398b282a997f078dd376cf5ef99a8b8160a038a
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-14 11:55:08 +0530

    Wrapped In A Arrow Function

--- Bubble Game/script.js
+++ Bubble Game/script.js
@@ -1,9 +1,14 @@
 // Event Bubbling Concept
-var clutter = "";
 
-for (var i = 1; i <= 234; i++) {
-  var randomNumber = Math.floor(Math.random() * 10);
-  clutter += `<div class="bubble">${randomNumber}</div>`;
-}
+const createBubble = () => {
+  var clutter = "";
 
-document.querySelector("#panelBottom").innerHTML = clutter;
+  for (var i = 1; i <= 234; i++) {
+    var randomNumber = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${randomNumber}</div>`;
+  }
+
+  document.querySelector("#panelBottom").innerHTML = clutter;
+};
+
+createBubble();
