e075584aefa99c0e148914792a7c8e3d89ac89eb
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-14 12:27:38 +0530

    Get Bubble And Event Bubble Concept Cleared

--- Bubble Game/script.js
+++ Bubble Game/script.js
@@ -1,6 +1,11 @@
-// Event Bubbling Concept
-
+// Event Bubbling Concept: On click the event will be raised on particular element.If the event listener not found on element then it will search on parent and if not found then it will research on parent's parent component and the loop will continue until the event not found. This concept is called event bubbling.
 var time = 60;
+var score = 0;
+
+const increaseScore = () => {
+  score += 10;
+  document.getElementById("scoreValue").textContent = score;
+};
 
 const createBubble = () => {
   var clutter = "";
@@ -26,5 +31,17 @@ const timer = () => {
   }, 1000);
 };
 
+const getNewHit = () => {
+  var newHit = Math.floor(Math.random() * 10);
+  document.querySelector("#hitValue").textContent = newHit;
+};
+
+document
+  .querySelector("#panelBottom")
+  .addEventListener("click", function (detail) {
+    console.log(Number(detail.target.textContent));
+  });
+
 createBubble();
 timer();
+getNewHit();
